988f82ccabffc0a8a6687c00ec6401a85bd132ce
Trần Nhật Duy <duytn.hcm@gmail.com>
2024-07-07 10:20:12 +0700

    Setup next

---
+++ frontend/src/app/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-[#f5f5f5] p-4 text-[#4f5355] text-center text-sm">
+      &copy; {new Date().getFullYear()} - Made with ❤ by Trần Nhật Duy
+    </footer>
+  );
+}
+
+export default Footer;
