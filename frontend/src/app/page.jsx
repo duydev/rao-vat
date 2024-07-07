bf4864d1b60eec4f084a233a21e005e8f66cf6e3
Trần Nhật Duy <duytn.hcm@gmail.com>
2024-07-07 13:38:41 +0700

    Fix bug

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,4 +1,10 @@
-function Home() {
+import { getAllArticles } from '@/app/apis/articles-api';
+
+async function Home() {
+  const articles = await getAllArticles();
+
+  console.log(articles);
+
   return <main className="max-w-[1140px] mx-auto my-4">Hello World</main>;
 }
 
